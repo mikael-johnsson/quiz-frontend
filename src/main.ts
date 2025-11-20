4ea090e7e7e1b6845c33ca5989396e1a7d4906f8
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2025-11-20 15:41:54 +0100

    add feature to let user chose theme, and only display questions if match

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import type { Question } from "./models/question";
 import type { QuestionResponse } from "./models/questionResponse";
 import "./style.css";
 import { clearHtml, createHtml } from "./utils/HtmlUtils";
 
+let chosenTheme: any; //fix this
+
 const fetchButton = document.getElementById("fetch-button");
+
 fetchButton?.addEventListener("click", () => {
   fetch("data/fakeDB.json")
     .then((res) => res.json())
@@ -11,9 +13,26 @@ fetchButton?.addEventListener("click", () => {
       clearHtml();
       const questions = data.questions;
 
-      for (let i = 0; i < 3; i++) {
-        let randomX = Math.floor(Math.random() * 10);
-        createHtml(questions[randomX]);
+      let chosenQuestions = questions.filter((question) =>
+        question.theme.includes(chosenTheme)
+      );
+
+      // display a random question three times
+      for (let i = 0; i < chosenQuestions.length; i++) {
+        let randomX = Math.floor(Math.random() * chosenQuestions.length);
+        createHtml(chosenQuestions[randomX]);
       }
     });
 });
+
+const checkTheme = () => {
+  const themeForm = document.getElementById("theme-form") as HTMLFormElement;
+
+  themeForm?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(themeForm);
+    chosenTheme = formData.get("theme");
+  });
+};
+
+checkTheme();
