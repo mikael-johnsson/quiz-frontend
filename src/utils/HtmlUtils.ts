152494330f003858638f7a89f9e711c6a4ad4b3c
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2025-11-20 14:39:07 +0100

    clear displayed questions before new ones displayed

--- src/utils/HtmlUtils.ts
+++ src/utils/HtmlUtils.ts
@@ -1,15 +1,22 @@
 import type { Question } from "../models/question";
 
 export const createHtml = (question: Question) => {
-  const questionContainer = document.createElement("div");
+  const questionsContainer = document.getElementById("questionsContainer");
+  const questionDiv = document.createElement("div");
   const title = document.createElement("p");
   const answer = document.createElement("span");
 
   title.innerHTML = question.question;
   answer.innerHTML = question.answer;
 
-  questionContainer.appendChild(title);
-  questionContainer.appendChild(answer);
-  const main = document.getElementById("main");
-  main?.appendChild(questionContainer);
+  questionDiv.appendChild(title);
+  questionDiv.appendChild(answer);
+  questionsContainer?.appendChild(questionDiv);
+};
+
+export const clearHtml = () => {
+  const questionsContainer = document.getElementById("questionsContainer");
+  if (questionsContainer) {
+    questionsContainer.innerHTML = "";
+  }
 };
