ba534fa9fc3255e0d31e54051cc99ad41d38d024
mikael-johnsson <mikaeljohanjohnsson@gmail.com>
2025-11-20 09:42:33 +0100

    create fetch data function

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import "./style.css";
+
+const fetchButton = document.getElementById("fetch-button");
+fetchButton?.addEventListener("click", () => {
+  fetch("data/fakeDB.json")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.questions[0]);
+    });
+});
